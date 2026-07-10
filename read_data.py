b59e8607e6f1f18949ce1e6437246d5363c8a5b1
mhassantrq <mhassantrq@gmail.com>
2026-07-10 21:05:10 +0500

    concat two dataframes

--- read_data.py
+++ read_data.py
@@ -3,3 +3,18 @@ import pandas as pd
 def read_csv():
     df = pd.read_csv('data/dataset02.csv')
     return df
+
+def read_data_merged():
+    df1 = pd.read_csv('data/dataset01.csv')
+    df2 = pd.read_csv('data/dataset02.csv')
+    df2['label'] = df2['label'].astype(int)
+
+    merged_df = pd.concat([df1, df2], ignore_index=True)
+
+    # print(df1.shape)
+    # print(df2.shape)
+    # print(merged_df.shape)
+    # print(merged_df.columns)
+    # print(merged_df['label'].unique())
+
+read_data_merged()
